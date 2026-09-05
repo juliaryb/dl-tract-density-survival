a37751864ee5661a2360a82a825603b73f26a7f5
Julia Rybska <rybska.julia@gmail.com>
2026-09-05 22:27:52 +0200

    added more channels to the CAE

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ class Encoder(nn.Module):
         D, H, W = input_shape
 
         self.net = nn.Sequential(
-            nn.Conv3d( 1,  8, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(8), nn.LeakyReLU(0.2),
-            nn.Conv3d( 8, 16, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(16), nn.LeakyReLU(0.2),
-            nn.Conv3d(16, 32, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(32), nn.LeakyReLU(0.2),
-            nn.Conv3d(32, 64, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(64), nn.LeakyReLU(0.2),
+            nn.Conv3d( 1,  32, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(32), nn.LeakyReLU(0.2),
+            nn.Conv3d( 32, 64, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(64), nn.LeakyReLU(0.2),
+            nn.Conv3d(64, 128, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(128), nn.LeakyReLU(0.2),
+            nn.Conv3d(128, 256, 3, stride=2, padding=1, bias=False), nn.BatchNorm3d(256), nn.LeakyReLU(0.2),
             nn.Flatten(),
-            nn.Linear(64 * (D // 16) * (H // 16) * (W // 16), latent_dim),
+            nn.Linear(256 * (D // 16) * (H // 16) * (W // 16), latent_dim),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -27,16 +27,16 @@ class Decoder(nn.Module):
         sd, sh, sw = D // 16, H // 16, W // 16
         self._spatial = (sd, sh, sw)
 
-        self.fc = nn.Linear(latent_dim, 64 * sd * sh * sw)
+        self.fc = nn.Linear(latent_dim, 256 * sd * sh * sw)
         self.net = nn.Sequential(
-            nn.ConvTranspose3d(64, 32, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(32), nn.LeakyReLU(0.2),
-            nn.ConvTranspose3d(32, 16, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(16), nn.LeakyReLU(0.2),
-            nn.ConvTranspose3d(16,  8, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(8), nn.LeakyReLU(0.2),
-            nn.ConvTranspose3d( 8,  1, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose3d(256, 128, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(128), nn.LeakyReLU(0.2),
+            nn.ConvTranspose3d(128, 64, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(64), nn.LeakyReLU(0.2),
+            nn.ConvTranspose3d(64,  32, 3, stride=2, padding=1, output_padding=1, bias=False), nn.BatchNorm3d(32), nn.LeakyReLU(0.2),
+            nn.ConvTranspose3d( 32,  1, 3, stride=2, padding=1, output_padding=1),
         )
 
     def forward(self, z: torch.Tensor) -> torch.Tensor:
-        x = self.fc(z).view(z.shape[0], 64, *self._spatial)
+        x = self.fc(z).view(z.shape[0], 256, *self._spatial)
         return self.net(x)
 
 
